76f42f2575fbc4f63abb9ba76f3c8c4cda34018f
Ben Gift <ben@weaveup.com>
2017-09-10 18:33:16 -0700

    docker_run starts Xquartz now

--- docker_run.sh
+++ docker_run.sh
@@ -1,6 +1,12 @@
+open -a Xquartz
+
 docker build -t hi .
+
 # start this socat thing that lets docker send data to the XQuartz
+# if it errors about the port in use then get the PID with this: lsof -i tcp:3000
 socat TCP-LISTEN:6000,reuseaddr,fork UNIX-CLIENT:\"$DISPLAY\" &
+
 # get the IP address for the display
 docker run -e DISPLAY=$((ifconfig en0 && ifconfig en1) | grep 'inet '|awk '{print $2}'):0 hi
+
 killall socat
